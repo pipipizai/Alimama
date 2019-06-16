e21d06b3c3f1179a35d63efd228e87278813f3f1
LING <179689727@qq.com>
2019-06-17 00:23:54 +0800

    crate iteminformation and favorite activity

---
+++ app/src/main/java/com/example/alimama/alimama/ui/activity/FavoriteActivity.java
@@ -0,0 +1,15 @@
+package com.example.alimama.alimama.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alimama.alimama.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
